24fc2986f8b609dd90a8bf3c8143b4604742e6f6
19023766 <19023766@DESKTOP-HHEUTM9>
2020-08-20 10:24:08 +0800

    updated Registration Class

--- C206_CaseStudy/src/Registration.java
+++ C206_CaseStudy/src/Registration.java
@@ -1,22 +1,32 @@
 
-public class Registration extends CourseSchedule{
+public class Registration {
 	
 	private String reg_id;
+	private String course_name;
 	
 	
 
 
 		// TODO Auto-generated constructor stub
 	public Registration(String course_name, String reg_id) {
-		super(course_name);
+		this.course_name = course_name;
 		this.reg_id = reg_id;
 	}
+	
+	
+
+
+	public String getCourse_name() {
+		return course_name;
+	}
+
+
 
 
 	public String getReg_id() {
 		return reg_id;
 	}
 	
-	//t 
+	
 
 }
