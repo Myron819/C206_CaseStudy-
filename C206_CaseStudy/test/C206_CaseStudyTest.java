6044ec23572deb3069640ac3fda322ad2d88a599
BuildTools <unconfigured@null.spigotmc.org>
2020-08-21 03:01:51 +0800

    Removed unused method

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -297,22 +297,4 @@ public class C206_CaseStudyTest {
 		
 	}
 	
-
-	
-	public static class IntegerAsker {
-	    private final Scanner scanner;
-	    private final PrintStream out;
-
-	    public IntegerAsker(InputStream in, PrintStream out) {
-	        scanner = new Scanner(in);
-	        this.out = out;
-	    }
-
-	    public int ask(String message) {
-	        out.println(message);
-	        return scanner.nextInt();
-	    }
-	}
-	
-	
 }
